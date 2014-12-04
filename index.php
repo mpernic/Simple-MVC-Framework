2e87f744dcd6a568e15e6e80deea68c302494ff0
mpernic <mpernic@hotmail.com>
2014-12-04 22:10:28 +0100

    Building the infrastructure

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
 
-require('views/view.php');
+define('DS', DIRECTORY_SEPARATOR);
+define('HOME', dirname(__FILE__));
 
-$view = new View('simple.tpl');
-$view->set('title', 'Naslov');
-$view->set('greeting', 'Pozdrav');
+ini_set('display_errors', 1);
 
-echo $view->output();
+require_once HOME . DS . 'config.php';
+require_once HOME . DS . 'utilities' . DS . 'bootstrap.php';
+
+function __autoload($class)
+{
+	if (file_exists(HOME . DS . 'utilities' . DS . strtolower($class) . '.php'))
+	{
+		require_once HOME . DS . 'utilities' . DS . strtolower($class) . '.php';
+	}
+	else if (file_exists(HOME . DS . 'models' . DS . strtolower($class) . '.php'))
+	{
+		require_once HOME . DS . 'models' . DS . strtolower($class) . '.php';
+	}
+	else if (file_exists(HOME . DS . 'controllers' . DS . strtolower($class) . '.php'))
+	{
+		require_once HOME . DS . 'controllers'  . DS . strtolower($class) . '.php';
+	}
+
 
 ?>
